fb7508278ab65222c7fede4eb408896ab3fc11ad
davidhrabcak <davidko.hrabcak@gmail.com>
2025-11-28 18:09:19 +0100

    implement totally wrong logic in Game, go insane

---
+++ src/main/java/com/terrafutura/api/TerraFuturaInterface.java
@@ -0,0 +1,24 @@
+package main.java.com.terrafutura.api;
+
+import main.java.com.terrafutura.board.GridPosition;
+import main.java.com.terrafutura.cards.Pair;
+import main.java.com.terrafutura.piles.CardSource;
+import main.java.com.terrafutura.piles.Deck;
+import main.java.com.terrafutura.resources.Resource;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TerraFuturaInterface {
+    boolean takeCard(int playerId, CardSource source, GridPosition destination);
+    boolean discardLastCardFromDeck(int playerId, Deck deck);
+    boolean activateCard(int playerId, GridPosition card,
+                         List<Pair<Resource, GridPosition>> inputs,
+                         List<Pair<Resource, GridPosition>> outputs,
+                         List<GridPosition> pollution,
+                         Optional<Integer> otherPlayerId, Optional<GridPosition> otherCard);
+    void selectReward(int playerId, Resource resource);
+    boolean turnFinished(int playerId);
+    boolean selectActivationPattern(int playerId, int card);
+    boolean selectScoring(int playerId, int card);
+}
--- src/main/java/com/terrafutura/game/Game.java
+++ src/main/java/com/terrafutura/game/Game.java
@@ -1,15 +1,20 @@
 package main.java.com.terrafutura.game;
 
-import main.java.com.terrafutura.board.ActivationPattern;
+import main.java.com.terrafutura.api.TerraFuturaInterface;
 import main.java.com.terrafutura.board.Grid;
+import main.java.com.terrafutura.board.GridPosition;
+import main.java.com.terrafutura.cards.Pair;
 import main.java.com.terrafutura.piles.ActivationPatterns;
 import main.java.com.terrafutura.piles.CardSource;
+import main.java.com.terrafutura.piles.Deck;
 import main.java.com.terrafutura.piles.ScoringMethods;
+import main.java.com.terrafutura.resources.Resource;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-public class Game {
+public class Game implements TerraFuturaInterface {
     public GameState state;
     public final List<Player> players;
     private int onTurn, startingPlayer, turnNumber;
@@ -18,7 +23,7 @@ public class Game {
     public Game(int playerNumber, int startingPlayerIndex) {
         state = null;
         this.players = new ArrayList<>();
-        for (int i = 1; i <= playerNumber; i++) {
+        for (int i = 0; i < playerNumber; i++) {
             Player p = setupPlayer(i);
             players.add(p);
             onTurn = startingPlayerIndex;
@@ -32,5 +37,41 @@ public class Game {
         ActivationPatterns a = new ActivationPatterns(g);
         ScoringMethods s = new ScoringMethods();
         return new Player(id, s.getRandomScoringMethod(), s.getRandomScoringMethod(), a.getRandomActivationPattern(), a.getRandomActivationPattern(), g);
+    }
+
+    @Override
+    public boolean takeCard(int playerId, CardSource source, GridPosition destination) {
+        Player p = players.get(playerId);
+        source.deck.
+    }
+
+    @Override
+    public boolean discardLastCardFromDeck(int playerId, Deck deck) {
+        return false;
+    }
+
+    @Override
+    public boolean activateCard(int playerId, GridPosition card, List<Pair<Resource, GridPosition>> inputs, List<Pair<Resource, GridPosition>> outputs, List<GridPosition> pollution, Optional<Integer> otherPlayerId, Optional<GridPosition> otherCard) {
+        return false;
+    }
+
+    @Override
+    public void selectReward(int playerId, Resource resource) {
+
+    }
+
+    @Override
+    public boolean turnFinished(int playerId) {
+        return false;
+    }
+
+    @Override
+    public boolean selectActivationPattern(int playerId, int card) {
+        return false;
+    }
+
+    @Override
+    public boolean selectScoring(int playerId, int card) {
+        return false;
     }
 }
